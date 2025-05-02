73b4551c6f235e1a71880a6c6285d7837d065310
GaboMAGARIA123 <gumbaridzegabrieli@gmail.com>
2025-05-02 16:14:45 +0400

    inital commit

--- script.js
+++ script.js
@@ -88,24 +88,7 @@ document.getElementById("productList").addEventListener("click", async function 
 });
 
 modalBtn.addEventListener("click", async function () {
-    const div = document.createElement("div");
-
-    div.className = "Product-list";
-
-    div.innerHTML = `
-    <img src="${document.getElementById("productImage").value}" alt="Product Image">
-    <h2>${document.getElementById("productName").value}</h2>
-    <span class="Price">$${document.getElementById("productPrice").value}</span>
-    <span class="Description">${document.getElementById("productDescription").value}</span>
-    <div class="btns">
-        <button id="editBtn">Edit</button>
-        <button id="deleteBtn">Delete</button>
-    </div>`;
-
-    document.getElementById("productList").appendChild(div);
-
-    productModal.style.display = "none";
-
+    
     const dataToSend = {
         image: document.getElementById("productImage").value,
         name: document.getElementById("productName").value,
@@ -113,23 +96,44 @@ modalBtn.addEventListener("click", async function () {
         description: document.getElementById("productDescription").value
     };
 
+    if (editproductid) {
+        const response = await fetch(`http://localhost:3000/api/products/${editproductid}`, {
+            method: 'PUT',
+            body: JSON.stringify(dataToSend),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (response.ok) {
+            editproductid = null;
+            productModal.style.display = "none";
+            document.getElementById("productList").innerHTML = "";
+            fetchItems();
+        } else {
+            console.error('Failed to update product:', response.statusText);
+        }
+    } else {
+
+        const response = await fetch('http://localhost:3000/api/products', {
+            method: 'POST',
+            body: JSON.stringify(dataToSend),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (response.ok) {
+            productModal.style.display = "none";
+            document.getElementById("productList").innerHTML = "";
+            fetchItems();
+        } else {
+            console.error('Failed to create product:', response.statusText);
+        }
+    }
+
     document.getElementById("productImage").value = "";
     document.getElementById("productName").value = "";
     document.getElementById("productPrice").value = "";
     document.getElementById("productDescription").value = "";
-
-    const response = await fetch('http://localhost:3000/api/products', {
-        method: 'POST',
-        body: JSON.stringify(dataToSend),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-    if (response.ok) {
-        productModal.style.display = "none";
-        document.getElementById("productList").innerHTML = "";
-        fetchItems();
-    }
 });
 
 closeModalBtn.addEventListener("click", function () {
